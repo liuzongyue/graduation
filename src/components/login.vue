14b35db33be98da491c6aabac0e25ac75a4097ed
liuzongyue <2559221072@qq.com>
2021-02-04 09:40:26 +0800

    初始化毕业设计

---
+++ src/components/login.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="main-login">
+    <div class="login">
+      <div class="name">
+        <span style="width:15%;text-align:end;margin-right:20px">phone:</span><el-input v-model="input_name" placeholder="Please enter your cell phone number"></el-input>
+      </div>
+      <div class="name password">
+        <span style="width:15%;text-align:end;margin-right:20px">password:</span><el-input v-model="input_password" placeholder="Please enter your password"></el-input>
+      </div>
+      <el-button type="primary" plain class="btn_login" @click="tryto">login</el-button>
+      <i @click="registered">/register</i>
+      <h5>Welcome to use</h5>
+    </div>
+  </div> 
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      input_name:"",
+      input_password:"",
+    }
+  },
+  methods:{
+    tryto(){
+    },
+    registered(){
+      this.$router.push({
+        path:'/register'
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scope>
+.main-login{
+  height: 100%;
+  background: url("./../assets/message/background_them.jpg") no-repeat;
+  background-size: 100% 100%;
+  background-attachment: scroll;
+  display: flex;
+  .login{
+    width: 25%;
+    background-image: url('./../assets/message/login.jpg');
+    background-size: 100% 100%;
+    opacity: 0.7;
+    .name{
+      margin-top: 10%;
+      text-align: center;
+      span{
+        display: inline-block;
+        margin: 0;
+      }
+      .el-input{
+        width: 50%;
+      }
+    }
+    .password{
+      margin-top: 5%;
+    }
+    h5{
+      margin-top: 5%;
+      text-align: center;
+      margin-bottom: 10%;
+    }
+    i{
+      display: inline-block;
+      margin-left: 20px;
+      font-size: 12px;
+      color: #b3d8ff;
+      cursor: pointer;
+    }
+    i:hover{
+      color: blue;
+    }
+    i:active{
+      color: #b3d8ff;
+    }
+    .btn_login{
+      width: 20%;
+      margin-top: 15%;
+      margin-left: 40%;
+    }
+  }
+}
+</style>
---
+++ src/components/register.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="main-register">
+    <div class="register">
+      <div class="name">
+        <span style="width:15%;text-align:end;margin-right:20px">phone:</span><el-input v-model="input_name" placeholder="Please enter your cell phone number"></el-input>
+      </div>
+      <div class="name password">
+        <span style="width:15%;text-align:end;margin-right:20px">username:</span><el-input v-model="input_username" placeholder="Please enter your name"></el-input>
+      </div>
+      <div class="name password">
+        <span style="width:15%;text-align:end;margin-right:20px">password:</span><el-input v-model="input_password" placeholder="Please enter your password"></el-input>
+      </div>
+      <div class="name password">
+        <span style="width:15%;text-align:end;margin-right:20px">cf-pass:</span><el-input v-model="input_confirm_password" placeholder="Please enter your password again"></el-input>
+      </div>
+      <el-button plain class="btn_register" @click="registered">register</el-button>
+      <h5>Welcome to use</h5>
+    </div>
+  </div> 
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      input_name:"",
+      input_username:"",
+      input_password:"",
+      input_confirm_password:"",
+      registeredft:false,
+    }
+  },
+  methods:{
+    registered(){
+      this.$router.push({
+        path:'/login'
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scope>
+.main-register{
+  height: 100%;
+  background: url("./../assets/message/background_them.jpg") no-repeat;
+  background-size: 100% 100%;
+  background-attachment: scroll;
+  display: flex;
+  .register{
+    width: 25%;
+    background-image: url('./../assets/message/register.jpg');
+    opacity: 0.7;
+    .name{
+      margin-top: 10%;
+      text-align: center;
+      span{
+        display: inline-block;
+        margin: 0;
+      }
+      .el-input{
+        width: 50%;
+      }
+    }
+    .password{
+      margin-top: 5%;
+    }
+    h5{
+      margin-top: 5%;
+      text-align: center;
+      margin-bottom: 10%;
+    }
+    .btn_register{
+      width: 20%;
+      margin-top: 15%;
+      margin-left: 40%;
+    }
+  }
+}
+</style>
